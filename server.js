67511f089af895a6292007d5c0f6d584ae55a976
Dusan Randjelovic <dusanrandjelovic92@gmail.com>
2019-10-07 19:52:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 // za publikovanje
-/*if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}*/
+}
 
 const port = process.env.PORT || 5000;
 
